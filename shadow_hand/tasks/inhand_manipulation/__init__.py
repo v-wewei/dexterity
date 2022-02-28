ccc49cad59a73621a945d66ada84632eeeb4142b
Kevin Zakka <kevinarmandzakka@gmail.com>
2022-02-27 18:52:49 -0800

    Add mujoco collision filtering after task has been constructed but before feeding to dm_control.Environment.

--- shadow_hand/tasks/inhand_manipulation/__init__.py
+++ shadow_hand/tasks/inhand_manipulation/__init__.py
@@ -6,6 +6,7 @@ from dm_control import composer as _composer
 
 from shadow_hand.tasks.inhand_manipulation import reorient as _reorient
 from shadow_hand.tasks.inhand_manipulation.shared import registry as _registry
+from shadow_hand.utils import mujoco_collisions
 
 _registry.done_importing_tasks()
 
@@ -14,5 +15,12 @@ ALL: Tuple[str, ...] = tuple(_registry.get_all_names())
 
 
 def load(environment_name: str, seed: Optional[int] = None) -> _composer.Environment:
-    task = _registry.get_constructor(environment_name)()
+    # Build the task.
+    task: _composer.Task = _registry.get_constructor(environment_name)()
+
+    # Ensure MuJoCo will not check for collisions between geoms that can never collide.
+    mujoco_collisions.exclude_bodies_based_on_contype_conaffinity(
+        task.root_entity.mjcf_model
+    )
+
     return _composer.Environment(task=task, random_state=seed)
