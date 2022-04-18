d95f369f07c9399dae30ca2383888681a35bef9f
Kevin <kevinarmandzakka@gmail.com>
2022-04-18 16:57:07 -0700

    [wip] path fix.

--- dexterity/__init__.py
+++ dexterity/__init__.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 # Path to the root of the project.
-_PROJECT_ROOT: Path = Path(__file__).parent.parent
+_PROJECT_ROOT: Path = Path(__file__).absolute().parent.parent
 
 # Path to the root of the src files, i.e. `dexterity/`.
-_SRC_ROOT: Path = Path(__file__).parent.parent / "dexterity"
+_SRC_ROOT: Path = _PROJECT_ROOT / "dexterity"
