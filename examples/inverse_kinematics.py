a8679c547e20ac53752be9a9ecbf2f5325fe0438
Kevin Zakka <kevinarmandzakka@gmail.com>
2022-04-21 16:35:42 -0700

    Cleanup inverse kinematics example.

--- examples/inverse_kinematics.py
+++ examples/inverse_kinematics.py
@@ -6,16 +6,14 @@ import numpy as np
 from absl import app
 from absl import flags
 from dm_control import mjcf
-from dm_control import mujoco
-from dm_control.mujoco import wrapper
 from dm_robotics.transformations import transformations as tr
 from matplotlib import pyplot as plt
 
 from dexterity.inverse_kinematics import ik_solver
-from dexterity.manipulation.arenas import Standard
+from dexterity.manipulation.arenas import Empty
 from dexterity.manipulation.shared import cameras
+from dexterity.manipulation.shared import workspaces
 from dexterity.models.hands import shadow_hand_e
-from dexterity.models.hands import shadow_hand_e_constants as consts
 
 flags.DEFINE_integer("seed", None, "Random seed.")
 flags.DEFINE_integer("num_solves", 1, "Number of IK solves.")
@@ -24,88 +22,83 @@ flags.DEFINE_boolean("disable_plot", False, "Angular tolerance.")
 
 FLAGS = flags.FLAGS
 
-
-def render_scene(
-    physics: mjcf.Physics,
-    transparent: bool = False,
-    cam_id: str = cameras.FRONT_CLOSE.name,
-) -> np.ndarray:
-    scene_option = wrapper.core.MjvOption()
-    scene_option.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = transparent
-    return physics.render(
-        width=640,
-        height=480,
-        camera_id=cam_id,
-        scene_option=scene_option,
-    )
+_SITE_SIZE = 1e-2
+_SITE_ALPHA = 0.1
+_SITE_COLORS = (
+    (1.0, 0.0, 0.0),  # Red.
+    (0.0, 1.0, 0.0),  # Green.
+    (0.0, 0.0, 1.0),  # Blue.
+    (0.0, 1.0, 1.0),  # Cyan.
+    (1.0, 0.0, 1.0),  # Magenta.
+)
+_TARGET_SIZE = 5e-3
+_TARGET_ALPHA = 1.0
 
 
 def main(_) -> None:
-    if FLAGS.seed is not None:
-        np.random.seed(FLAGS.seed)
+    random_state = np.random.RandomState(seed=FLAGS.seed)
+
+    # Build the scene.
+    arena = Empty()
+    axis_angle = np.radians(180) * np.array([0, np.sqrt(2) / 2, -np.sqrt(2) / 2])
+    quat = tr.axisangle_to_quat(axis_angle)
+    hand = shadow_hand_e.ShadowHandSeriesE()
+    arena.attach_offset(hand, position=(0, 0.2, 0.1), quaternion=quat)
+
+    # Add camera.
+    arena.mjcf_model.worldbody.add(
+        "camera",
+        name=cameras.FRONT_CLOSE.name,
+        pos=cameras.FRONT_CLOSE.pos,
+        xyaxes=cameras.FRONT_CLOSE.xyaxes,
+    )
+
+    solver = ik_solver.IKSolver(model=arena.mjcf_model, prefix=hand.mjcf_model.model)
+
+    # Create target sites for each fingertip.
+    target_sites = []
+    for i, site in enumerate(hand.fingertip_sites):
+        target_sites.append(
+            workspaces.add_target_site(
+                body=arena.mjcf_model.worldbody,
+                radius=_TARGET_SIZE,
+                visible=True,
+                rgba=_SITE_COLORS[i] + (_TARGET_ALPHA,),
+                name=f"target_{site.name}",
+            )
+        )
+
+    # Customize hand fingertip sites.
+    for i, site in enumerate(hand.fingertip_sites):
+        site.group = None  # Make the sites visible.
+        site.size = (_SITE_SIZE,) * 3  # Increase their size.
+        site.rgba = _SITE_COLORS[i] + (_SITE_ALPHA,)  # Change their color.
+
+    render_kwargs = dict(width=640, height=480, camera_id=0)
 
     successes: int = 0
     for _ in range(FLAGS.num_solves):
-        # Build the scene.
-        arena = Standard()
-        axis_angle = np.radians(180) * np.array([0, np.sqrt(2) / 2, -np.sqrt(2) / 2])
-        quat = tr.axisangle_to_quat(axis_angle)
-        hand = shadow_hand_e.ShadowHandSeriesE()
-        arena.attach_offset(hand, position=(0, 0.2, 0.1), quaternion=quat)
-
-        # Add camera.
-        arena.mjcf_model.worldbody.add(
-            "camera",
-            name=cameras.FRONT_CLOSE.name,
-            pos=cameras.FRONT_CLOSE.pos,
-            xyaxes=cameras.FRONT_CLOSE.xyaxes,
-        )
-
-        # The fingers we'd like to control.
-        fingers = (
-            consts.Components.FF,
-            consts.Components.MF,
-            consts.Components.RF,
-            consts.Components.LF,
-            consts.Components.TH,
-        )
-
-        solver = ik_solver.IKSolver(
-            model=arena.mjcf_model,
-            fingers=fingers,
-            prefix=hand.mjcf_model.model,
-        )
-
-        # Set the configuration and query fingertip sites.
         physics = mjcf.Physics.from_mjcf_model(arena.mjcf_model)
-        for finger in fingers:
-            cntrl_jnts = solver._controllable_joints[finger]
-            joint_binding = physics.bind(cntrl_jnts)
-            joint_binding.qpos[:] = np.random.uniform(
-                joint_binding.range[:, 0],
-                joint_binding.range[:, 1],
-            )
+
+        # Randomly sample a joint configuration.
+        qpos_desired = hand.sample_joint_angles(physics, random_state)
+        qpos_initial = physics.bind(hand.joints).qpos.copy()
+        physics.bind(hand.joints).qpos[:] = qpos_desired
+
+        # Forward kinematics to compute Cartesian fingertip positions.
         target_positions = {}
         for finger, fingertip_site in hand._fingertip_site_elem_mapping.items():
-            if finger in fingers:
-                target_positions[finger] = physics.bind(fingertip_site).xpos.copy()
-        assert set(tuple(target_positions.keys())) == set(fingers)
-        im_desired = render_scene(physics, transparent=False)
+            target_positions[finger] = physics.bind(fingertip_site).xpos.copy()
 
-        # Add the target sites to the MJCF model for visualization purposes.
-        for name, position in target_positions.items():
-            arena.mjcf_model.worldbody.add(
-                "site",
-                name=f"{name}_target",
-                type="sphere",
-                pos=position,
-                rgba="0 0 1 .7",
-                size="0.005",
-            )
+        # Set target sites to their respective locations.
+        physics.bind(target_sites).pos = np.array(list(target_positions.values()))
 
-        # Recreate physics instance since we changed the MJCF.
-        physics = mjcf.Physics.from_mjcf_model(arena.mjcf_model)
-        im_start = render_scene(physics, transparent=False)
+        # Restore joints to their initial configuration.
+        physics.bind(hand.joints).qpos[:] = qpos_initial
+
+        # Forward dynamics to update all the positions.
+        physics.forward()
+        im_start = physics.render(**render_kwargs)
 
         ik_start = time.time()
         qpos = solver.solve(
@@ -119,25 +112,19 @@ def main(_) -> None:
         ik_end = time.time()
 
         if qpos is not None:
-            solve_time_ms = (ik_end - ik_start) * 1000
-            print(f"Full IK solved in {solve_time_ms} ms.")
+            print(f"Full IK solved in {(ik_end - ik_start) * 1000} ms.")
 
             # Directly set joint angles and visualize.
             hand.set_joint_angles(physics, qpos)
-            physics.step()
-            im_actual = render_scene(physics, transparent=False)
-            im_actual_tr = render_scene(physics, transparent=True)
+            physics.forward()
+            im_actual = physics.render(**render_kwargs)
 
             if not FLAGS.disable_plot:
-                _, axes = plt.subplots(1, 4, figsize=(12, 4))
+                _, axes = plt.subplots(1, 2, figsize=(12, 4))
                 axes[0].imshow(im_start)
                 axes[0].set_title("Starting")
-                axes[1].imshow(im_desired)
-                axes[1].set_title("Desired")
-                axes[2].imshow(im_actual)
-                axes[2].set_title("Actual")
-                axes[3].imshow(im_actual_tr)
-                axes[3].set_title("Actual (transparent)")
+                axes[1].imshow(im_actual)
+                axes[1].set_title("Solution")
                 for ax in axes:
                     ax.axis("off")
                 plt.subplots_adjust(wspace=0, hspace=0)
@@ -145,11 +132,6 @@ def main(_) -> None:
                 plt.show()
 
             successes += 1
-        else:
-            plt.figure("failed")
-            plt.imshow(im_desired)
-            plt.axis("off")
-            plt.show()
 
     print(f"solve success rate: {successes}/{FLAGS.num_solves}.")
 
