3130b2fde9c88f1e5f2853ec318cfaa9af543888
Kevin Zakka <kevinarmandzakka@gmail.com>
2022-04-25 23:20:48 -0700

    Create spec_utils module.

--- dexterity/task.py
+++ dexterity/task.py
@@ -10,6 +10,7 @@ from dm_env import specs
 from dexterity import effector
 from dexterity import goal
 from dexterity.models.hands import dexterous_hand
+from dexterity.utils import spec_utils
 
 
 class Task(composer.Task):
@@ -68,7 +69,7 @@ class Task(composer.Task):
         if self._action_spec is None:
             # Merge action specs.
             a_specs = [a.action_spec(physics) for a in self._hand_effectors]
-            self._action_spec = merge_specs(a_specs)
+            self._action_spec = spec_utils.merge_specs(a_specs)
         return self._action_spec
 
     # Accessors.
@@ -217,34 +218,3 @@ class GoalTask(Task):
     @property
     def successes_needed(self) -> int:
         return self._successes_needed
-
-
-# Reference: https://github.com/deepmind/dm_robotics/blob/main/py/agentflow/spec_utils.py
-def merge_specs(spec_list: Sequence[specs.BoundedArray]):
-    """Merges a list of `BoundedArray` specs into one."""
-
-    # Check all specs are flat.
-    for spec in spec_list:
-        if len(spec.shape) > 1:
-            raise ValueError("Not merging multi-dimensional spec: {}".format(spec))
-
-    # Filter out no-op specs with no actuators.
-    spec_list = [spec for spec in spec_list if spec.shape and spec.shape[0]]
-    dtype = np.find_common_type([spec.dtype for spec in spec_list], [])
-
-    num_actions = 0
-    name = ""
-    mins = np.array([], dtype=dtype)
-    maxs = np.array([], dtype=dtype)
-
-    for i, spec in enumerate(spec_list):
-        num_actions += spec.shape[0]
-        if name:
-            name += "\t"
-        name += spec.name or f"spec_{i}"
-        mins = np.concatenate([mins, spec.minimum])
-        maxs = np.concatenate([maxs, spec.maximum])
-
-    return specs.BoundedArray(
-        shape=(num_actions,), dtype=dtype, minimum=mins, maximum=maxs, name=name
-    )
---
+++ dexterity/utils/spec_utils.py
@@ -0,0 +1,37 @@
+"""Utilities for working with dm_env specs."""
+
+from typing import Sequence
+
+import numpy as np
+from dm_env import specs
+
+
+# Reference: https://github.com/deepmind/dm_robotics/blob/main/py/agentflow/spec_utils.py
+def merge_specs(spec_list: Sequence[specs.BoundedArray]) -> specs.BoundedArray:
+    """Merges a list of `BoundedArray` specs into one."""
+
+    # Check all specs are flat.
+    for spec in spec_list:
+        if len(spec.shape) > 1:
+            raise ValueError("Not merging multi-dimensional spec: {}".format(spec))
+
+    # Filter out no-op specs with no actuators.
+    spec_list = [spec for spec in spec_list if spec.shape and spec.shape[0]]
+    dtype = np.find_common_type([spec.dtype for spec in spec_list], [])
+
+    num_actions = 0
+    name = ""
+    mins = np.array([], dtype=dtype)
+    maxs = np.array([], dtype=dtype)
+
+    for i, spec in enumerate(spec_list):
+        num_actions += spec.shape[0]
+        if name:
+            name += "\t"
+        name += spec.name or f"spec_{i}"
+        mins = np.concatenate([mins, spec.minimum])
+        maxs = np.concatenate([maxs, spec.maximum])
+
+    return specs.BoundedArray(
+        shape=(num_actions,), dtype=dtype, minimum=mins, maximum=maxs, name=name
+    )
